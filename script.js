f01c2ddbe2542de944a1ad385232cc124a7db1b4
Nguyen Minh Thuan <0023412724@student.dthu.edu.vn>
2026-03-01 21:37:51 +0700

    Day8

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-let inputTen = document.getElementById("ten");
-let buttonGui = document.getElementById("gui");
-let loiChao = document.getElementById("loiChao");
+let kyNang =["HTML","CSS","Flexbox"];
 
-buttonGui.addEventListener("click", function() {
-    let ten= inputTen.value.trim();
-    
-if (ten === "") {
-    loiChao.textContent = "Vui lòng nhập tên của bạn.";
-    loiChao.style.color = "red";
-} else{
-    loiChao.textContent = "Xin chào, " + ten + "!";
-    loiChao.style.color = "green";
-    inputTen.value="";
+let saved= localStorage.getItem("kyNang");
+if(saved){
+    kyNang=JSON.parse(saved);
 }
+
+let danhSach = document.getElementById("kyNangList");
+let input = document.getElementById("kyNangInput");
+let buttonThem = document.getElementById("themKyNang");
+
+function hienThiKyNang(){
+    danhSach.innerHTML = "";
+    kyNang.forEach(function(item){
+    let li = document.createElement("li");
+    li.textContent = item;
+    danhSach.appendChild(li);  
+
+    li.addEventListener("click", function(){
+        kyNang = kyNang.filter(ky => ky !== item);
+        localStorage.setItem("kyNang", JSON.stringify(kyNang));
+        hienThiKyNang();
+        console.log("Đã xoá: " + item)
+        });
+    });
+}
+
+hienThiKyNang();
+buttonThem.addEventListener("click", function(){
+    let kyNangMoi = input.value.trim();
+    if (kyNangMoi !== "") {
+        kyNang.push(kyNangMoi);
+        localStorage.setItem("kyNang", JSON.stringify(kyNang));
+        hienThiKyNang();
+        console.log("Đã thêm: " + kyNangMoi);
+        input.value = "";
+    } else {
+        alert("Vui lòng nhập kỹ năng!");
+    }
 });
